ae3eb2c34b4ba0c12934ec1a7ee8c7e224c8ab16
369px <168459560+369px@users.noreply.github.com>
2024-10-12 01:32:29 +0200

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -9,48 +9,59 @@ files_modified = 0
 for console in os.listdir(roms_dir):
     console_path = os.path.join(roms_dir, console)
     
-    # Procedi solo se è una directory
     if os.path.isdir(console_path):
         json_path = os.path.join(console_path, 'games.json')
         readme_path = os.path.join(console_path, 'README.md')
         
-        # Processa solo se esiste games.json
         if os.path.exists(json_path):
             with open(json_path) as f:
-                games = json.load(f)
-            
-            # Costruisci il contenuto del README
-            content = f"# Check out these games for {console}!\n\n"
-            for game in games:
-                content += f'<img width="123" src="{game["cover"]}">\n'
-                content += '<div style="background-color:#eeeeee">\n'
-                content += f'<details>\n  <summary>{game["name"]}</summary>\n  <br>\n'
-                content += '  <i>Recommended by:</i>\n  <br>\n'
-                
-                # Aggiungi ogni collaboratore con link GitHub e avatar
-                for user in game['recommended_by']:
-                    content += f'  <a href="https://github.com/{user}/">\n'
-                    content += f'  <img src="https://avatars.githubusercontent.com/{user}?s=24" align="left"/></a> {user}\n  <br>\n'
-                
-                content += '  <br></details></div>\n\n'
-            
-            # Aggiorna o crea il README.md
-            if os.path.exists(readme_path):
-                with open(readme_path, 'r') as f:
-                    existing_content = f.read()
-                
-                if content != existing_content:
-                    with open(readme_path, 'w') as f:
-                        f.write(content)
-                    print(f"Updated README.md for {console}.")
-                    files_modified += 1
-            else:
-                with open(readme_path, 'w') as f:
-                    f.write(content)
-                print(f"Created README.md for {console}.")
-                files_modified += 1
+                data = json.load(f)
 
-# Controlla se sono stati modificati file
+            # Accedi alla lista di giochi usando la chiave "games"
+            games = data.get("games", [])
+            
+            if isinstance(games, list):
+                content = f"# Check out these games for {console}!\n\n"
+                
+                for game in games:
+                    if isinstance(game, dict):  # Verifica che game sia un dizionario
+                        # Controlla la presenza dei campi necessari
+                        cover = game.get("cover")
+                        name = game.get("name")
+                        recommended_by = game.get("recommended_by", [])
+
+                        if cover and name:
+                            content += f'<img width="123" src="{cover}">\n'
+                            content += '<div style="background-color:#eeeeee">\n'
+                            content += f'<details>\n  <summary>{name}</summary>\n  <br>\n'
+                            content += '  <i>Recommended by:</i>\n  <br>\n'
+                            
+                            for user in recommended_by:
+                                content += f'  <a href="https://github.com/{user}/">\n'
+                                content += f'  <img src="https://avatars.githubusercontent.com/{user}?s=24" align="left"/></a> {user}\n  <br>\n'
+                            
+                            content += '  <br></details></div>\n\n'
+                    else:
+                        print(f"Elemento non valido in games.json per {console}: {game}")
+
+                if content:
+                    if os.path.exists(readme_path):
+                        with open(readme_path, 'r') as f:
+                            existing_content = f.read()
+                        
+                        if content != existing_content:
+                            with open(readme_path, 'w') as f:
+                                f.write(content)
+                            print(f"Updated README.md for {console}.")
+                            files_modified += 1
+                    else:
+                        with open(readme_path, 'w') as f:
+                            f.write(content)
+                        print(f"Created README.md for {console}.")
+                        files_modified += 1
+            else:
+                print(f"Il file {json_path} non contiene una lista di giochi.")
+
 if files_modified == 0:
     raise ValueError("No README.md files were updated.")
 else:
